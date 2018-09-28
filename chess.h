c9c3b3268a3ace9158433ab0557981912886ae66
Sylvain Lacombe <slacombe.ti@gmail.com>
2018-09-28 12:33:13 -0400

    format with tabs to make it easier to read.

--- chess.h
+++ chess.h
@@ -18,7 +18,7 @@ typedef unsigned __int64 int64;
 typedef unsigned long long int64;
 #endif
 
-typedef int64	Bitboard;
+typedef int64 Bitboard;
 
 #define    ROI             6
 #define    DAME            5
@@ -35,20 +35,20 @@ typedef int64	Bitboard;
 #define MAXPLY      60
 
 // Definition pour la librairie d'ouverture.
-struct BOOKPOSITION
-{
-  Bitboard hashkey;       // Cle pour identifie la position.
-  // 8 premier bit. Qualite de la position.
-  // 0000 0001  Tres mauvais coup.
-  // 0000 0010  Mauvais coup.
-  // 0000 0100  Coup normal.
-  // 0000 1000  Bon coup.
-  // 0001 0000  Tres bon coup.
-  // 0010 0000  Les noirs ont gagne au moins une partie.
-  // 0100 0000  Au moins un partie nulle.
-  // 1000 0000  Les blancs ont gagne au moins une partie.
-  // 24 bit suivant. Nombre de fois joue.
-  unsigned int  flag;
+
+struct BOOKPOSITION {
+	Bitboard hashkey; // Cle pour identifie la position.
+	// 8 premier bit. Qualite de la position.
+	// 0000 0001  Tres mauvais coup.
+	// 0000 0010  Mauvais coup.
+	// 0000 0100  Coup normal.
+	// 0000 1000  Bon coup.
+	// 0001 0000  Tres bon coup.
+	// 0010 0000  Les noirs ont gagne au moins une partie.
+	// 0100 0000  Au moins un partie nulle.
+	// 1000 0000  Les blancs ont gagne au moins une partie.
+	// 24 bit suivant. Nombre de fois joue.
+	unsigned int flag;
 };
 
 #define  POSITION_PER_SLOT     600
@@ -65,91 +65,90 @@ typedef int SCORE;
 #define        VALDAME         900
 #define        VALROI          2000
 
-#define			BOOKMOVE		100000
+#define   BOOKMOVE  100000
 
-#define        ROQUEROIBLANC   0x01
-#define        ROQUEDAMEBLANC  0x02
-#define        ROQUEROINOIR    0x04
-#define        ROQUEDAMENOIR   0x08
+#define		ROQUEROIBLANC   0x01
+#define		ROQUEDAMEBLANC  0x02
+#define		ROQUEROINOIR    0x04
+#define		ROQUEDAMENOIR   0x08
 
-#define        PAS_EXTENSION         0
-#define        EXTENSION_ECHEC       1
-#define        EXTENSION_RECAPTURE   2
-#define        EXTENSION_PIONPASSE   3
-#define        EXTENSION_PROMOTION   4
+#define		PAS_EXTENSION         0
+#define		EXTENSION_ECHEC       1
+#define		EXTENSION_RECAPTURE   2
+#define		EXTENSION_PIONPASSE   3
+#define		EXTENSION_PROMOTION   4
 
-#define        NULLE_REPETITION             1
-#define        NULLE_MANQUE_DE_MATERIEL     2
-#define        NULLE_PAT                    3
-#define        NULLE_PAR_ACCORD             4
-#define        NULLE_REGLE_50COUPS          5
-#define		   DRAWSCORE					100
+#define		NULLE_REPETITION             1
+#define		NULLE_MANQUE_DE_MATERIEL     2
+#define		NULLE_PAT                    3
+#define		NULLE_PAR_ACCORD             4
+#define		NULLE_REGLE_50COUPS          5
+#define		DRAWSCORE     100
 
 #define         FORKUNCERTAIN                   50
 
 // Structure pour representer un move.
-struct TMove
-{
-  // Une structure de 64 bit.
-  // Utile pour pouvoir defaire le coup.
-  unsigned From      :  6;
-  unsigned To        :  6;
-  int Piece          :  4;   // La piece qui bouge.
-  int Capture        :  4;   // La piece capture.
-  unsigned Roque     :  1;   // Le roque.
-  unsigned EnPassant :  1;
-  unsigned Promotion :  4;   // 0 ou piece de la promotion.
-  int Reserve        :  6;
-  int Score;
+
+struct TMove {
+	// Une structure de 64 bit.
+	// Utile pour pouvoir defaire le coup.
+	unsigned From : 6;
+	unsigned To : 6;
+	int Piece : 4; // La piece qui bouge.
+	int Capture : 4; // La piece capture.
+	unsigned Roque : 1; // Le roque.
+	unsigned EnPassant : 1;
+	unsigned Promotion : 4; // 0 ou piece de la promotion.
+	int Reserve : 6;
+	int Score;
 };
 
 class TChessBoard;
 
 // Gestion d'une liste de coup.
-class TMoveList
-{
+
+class TMoveList {
 public:
-  int nbmove;
-  int currmove;
-  TMove moves[MAXMOVE];
+	int nbmove;
+	int currmove;
+	TMove moves[MAXMOVE];
 
-  TMoveList() {
-    nbmove = 0;
-    currmove = 0;
-  }
+	TMoveList() {
+		nbmove = 0;
+		currmove = 0;
+	}
 
-  void Copie( TMoveList& ml )
-  {
-    memcpy( moves, ml.moves, ml.nbmove*sizeof( TMove ) );
-  }
+	void Copie(TMoveList& ml) {
+		memcpy(moves, ml.moves, ml.nbmove * sizeof ( TMove));
+	}
 
-  void Ajoute( TMove& i_move ) {
-    moves[ nbmove++ ] = i_move;
-  }
+	void Ajoute(TMove& i_move) {
+		moves[ nbmove++ ] = i_move;
+	}
 
-  void Vide() {
-    nbmove = 0;
-    currmove = 0;
-  }
+	void Vide() {
+		nbmove = 0;
+		currmove = 0;
+	}
 
-  TMove& CurrentMove() {
-    return moves[currmove];
-  }
+	TMove& CurrentMove() {
+		return moves[currmove];
+	}
 
-  void Tri() {
-    if ( nbmove <= 0 )
-      return;
-    for( int i=0; i<nbmove-1; i++ )
-      for( int j=i+1; j<nbmove; j++ )
-        if ( moves[i].Score < moves[j].Score ) {
-          TMove move = moves[i];
-          moves[i] = moves[j];
-          moves[j] = move;
-        }
-  }
+	void Tri() {
+		if (nbmove <= 0)
+			return;
+		for (int i = 0; i < nbmove - 1; i++)
+			for (int j = i + 1; j < nbmove; j++)
+				if (moves[i].Score < moves[j].Score) {
+					TMove move = moves[i];
+					moves[i] = moves[j];
+					moves[j] = move;
+				}
+	}
 
-  void ChoisiMove(int ply, int wtm);
-  void ChoisiNonQuiet(int ply, int wtm);
+	void ChoisiMove(int ply, int wtm);
+	void ChoisiNonQuiet(int ply, int wtm);
 };
 
 
@@ -161,14 +160,15 @@ public:
 #define roi        6
 
 enum {
-  A8, B8, C8, D8, E8, F8, G8, H8,
-  A7, B7, C7, D7, E7, F7, G7, H7,
-  A6, B6, C6, D6, E6, F6, G6, H6,
-  A5, B5, C5, D5, E5, F5, G5, H5,
-  A4, B4, C4, D4, E4, F4, G4, H4,
-  A3, B3, C3, D3, E3, F3, G3, H3,
-  A2, B2, C2, D2, E2, F2, G2, H2,
-  A1, B1, C1, D1, E1, F1, G1, H1 };
+	A8, B8, C8, D8, E8, F8, G8, H8,
+	A7, B7, C7, D7, E7, F7, G7, H7,
+	A6, B6, C6, D6, E6, F6, G6, H6,
+	A5, B5, C5, D5, E5, F5, G5, H5,
+	A4, B4, C4, D4, E4, F4, G4, H4,
+	A3, B3, C3, D3, E3, F3, G3, H3,
+	A2, B2, C2, D2, E2, F2, G2, H2,
+	A1, B1, C1, D1, E1, F1, G1, H1
+};
 
 // Liste le dernier bit de toutes les combinaisons possibles
 // pour un entier de 16 bits. Donc 65536 possibilitees.
--- sortie.cpp
+++ sortie.cpp
@@ -15,99 +15,93 @@ extern bool xboard;
 extern int iMateInPly;
 
 // Prend un move et genere une chaine lisible.
-int SortieMove( TMove& move, char* i_szString )
-{
-  // La case source.
-  i_szString[0] = (char)(move.From % 8 + 'a');
-  i_szString[1] = (char)(8-(move.From >> 3)+'0');
 
-  // La case destination.
-  i_szString[2] = (char)(move.To % 8 + 'a');
-  i_szString[3] = (char)(8-(move.To >> 3)+'0');
+int SortieMove(TMove& move, char* i_szString) {
+	// La case source.
+	i_szString[0] = (char) (move.From % 8 + 'a');
+	i_szString[1] = (char) (8 - (move.From >> 3) + '0');
 
-  i_szString[4] = 0;
+	// La case destination.
+	i_szString[2] = (char) (move.To % 8 + 'a');
+	i_szString[3] = (char) (8 - (move.To >> 3) + '0');
 
-  return true;
+	i_szString[4] = 0;
+
+	return true;
 }
 
-void Sortie( const char* i_szReponse )
-{
-  if ( xboard ) {
-	printf( "move %s\n", i_szReponse );
-  }
-  else {
-	printf( "\nMy move %s\n", i_szReponse );
-  }
+void Sortie(const char* i_szReponse) {
+	if (xboard) {
+		printf("move %s\n", i_szReponse);
+	} else {
+		printf("\nMy move %s\n", i_szReponse);
+	}
 }
 
 // Obtient la continuation.
-void GetPV( char* o_szContinuation, TMove* ml, int ply )
-{
-  char szMove[6];
-  strcpy( o_szContinuation, "" );
-  int iNbMove = pv_length[ply];
-  for( int i=0; i<iNbMove; i++ ) {
-	  if (!(ml[i].From | ml[i].To))
-		  break;
-      SortieMove( ml[i], szMove );
-	  strcat( o_szContinuation, szMove );
-	  strcat( o_szContinuation, " " );
-  }
+
+void GetPV(char* o_szContinuation, TMove* ml, int ply) {
+	char szMove[6];
+	strcpy(o_szContinuation, "");
+	int iNbMove = pv_length[ply];
+	for (int i = 0; i < iNbMove; i++) {
+		if (!(ml[i].From | ml[i].To))
+			break;
+		SortieMove(ml[i], szMove);
+		strcat(o_szContinuation, szMove);
+		strcat(o_szContinuation, " ");
+	}
 }
 
-void postThinking(int profondeur, int score, int elapsed, int nodes, char* continuation)
-{
-  printf( "%d %d %d %d %s\n", profondeur, score, elapsed, nodes, continuation);
-  fflush(stdout);
+void postThinking(int profondeur, int score, int elapsed, int nodes, char* continuation) {
+	printf("%d %d %d %d %s\n", profondeur, score, elapsed, nodes, continuation);
+	fflush(stdout);
 }
 
 #ifdef DEBUG
 // Obtient la continuation.
-void GetCurrent( TChessBoard& cb, char* o_szPath, int deep )
-{
-  char szMove[6];
-  strcpy( o_szPath, "" );
-  for( int i=0; i<cb.MoveList[deep].nbmove; i++ ) {
-    SortieMove( cb.MoveList[deep].moves[i], szMove );
-    strcat( o_szPath, szMove );
-    strcat( o_szPath, " " );
-  }
+
+void GetCurrent(TChessBoard& cb, char* o_szPath, int deep) {
+	char szMove[6];
+	strcpy(o_szPath, "");
+	for (int i = 0; i < cb.MoveList[deep].nbmove; i++) {
+		SortieMove(cb.MoveList[deep].moves[i], szMove);
+		strcat(o_szPath, szMove);
+		strcat(o_szPath, " ");
+	}
 }
 #endif
 
-void OutBookInfo( char* buffer )
-{
-	strcpy( buffer, "Book - " );
-	for( int i=0; i<pv_length[0]; i++ ) {
+void OutBookInfo(char* buffer) {
+	strcpy(buffer, "Book - ");
+	for (int i = 0; i < pv_length[0]; i++) {
 		char szMove[10];
-		SortieMove( pv[0][i], szMove );
-		strcat( buffer, szMove );
-		strcat( buffer, ": " );
+		SortieMove(pv[0][i], szMove);
+		strcat(buffer, szMove);
+		strcat(buffer, ": ");
 		char szPercent[10];
-		sprintf( szPercent, "%d%%", pv[0][i].Score );
-		strcat( buffer, szPercent );
-		if ( i < pv_length[0]-1 ) {
-			strcat( buffer, ", " );
+		sprintf(szPercent, "%d%%", pv[0][i].Score);
+		strcat(buffer, szPercent);
+		if (i < pv_length[0] - 1) {
+			strcat(buffer, ", ");
 		}
 	}
 }
 
-void PrintBoard()
-{
+void PrintBoard() {
 	int i;
 
-	for(i=A8; i<=H1; i++) {
+	for (i = A8; i <= H1; i++) {
 		printf(" %2d", cb.board[i]);
-		if (i % 8 == 7) 
+		if (i % 8 == 7)
 			printf("\n");
 	}
 	printf("\n");
 }
 
-void PrintBitBoard(Bitboard bitboard)
-{
+void PrintBitBoard(Bitboard bitboard) {
 	int i;
-	for(i=A8; i<=H1; i++) {
+	for (i = A8; i <= H1; i++) {
 		printf("%llu", bitboard & mask[i]);
 		if (i % 8 == 7)
 			printf("\n");
